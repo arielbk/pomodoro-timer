b20794460c3091e5410f0cdd919e202c934d10ba
arielbk <buchwald.kearney@gmail.com>
2018-07-26 22:02:13 +0200

    updated about

--- src/Pomodoro/About.js
+++ src/Pomodoro/About.js
@@ -8,9 +8,11 @@ class About extends Component {
       <div>
         <div className="about-toggle" onClick={this.props.onAboutToggle}>about</div>
         <div className="about-pomodoro" style={this.props.styles}>
-          <p>The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.</p>
-          <p>The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, separated by short breaks.</p>
-          <p>These intervals are named pomodoros, the plural in English of the Italian word pomodoro (tomato), after the tomato-shaped kitchen timer that Cirillo used as a university student.</p>
+          <p>The Pomodoro Technique is a tool for time management and productivity.</p>
+          <p>Work for one 'pomodoro' (one set interval — usually 25 minutes) and then have a short break. After 4 of these, go and have a long break. It's stupidly simple, but extremely effective for focus and productivity.</p>
+          <p>Have you ever felt like you have so much to do that it is overwhelming, and you actually end up procrastinating? Set the timer for 25 minutes, and just begin. After all, its only 25 minutes!</p>
+          <p>Break your work up into intensely focused and proactive bursts. Go have a glass of water. Have a stretch. Jump around. Stare out the window. Then dive back in.</p>
+          <p>The Pomodoro Technique has been around since the 1980s, and was developed by a man named Francesco Cirillo. Find out more about it on the <a href="https://en.wikipedia.org/wiki/Pomodoro_Technique">Wikipedia page</a>.</p>
         <div className="close-btn-about" onClick={this.props.onAboutToggle}>✕</div>
         </div>
       </div>
